3952d3c45d789e68f0abd47ab8e5a60c2d479f22
Caitlin Bartley <cbartley@human-element.com>
2019-08-28 23:12:15 +0000

    changes for m23 support

--- view/frontend/web/js/catalog-add-to-cart.js
+++ view/frontend/web/js/catalog-add-to-cart.js
@@ -10,38 +10,75 @@ define([
 ], function($, $t) {
     "use strict";
 
-    $.widget('he.catalogAddToCart', $.mage.catalogAddToCart, {
-        ajaxSubmit: function(form) {
-            var self = this;
+    $.widget('mage.catalogAddToCart', $.mage.catalogAddToCart, {
+        ajaxSubmit: function (form) {
+            var self = this,
+                productIds = idsResolver(form),
+                formData;
+
             $(self.options.minicartSelector).trigger('contentLoading');
             self.disableAddToCartButton(form);
+            formData = new FormData(form[0]);
 
             $.ajax({
                 url: form.attr('action'),
-                data: form.serialize(),
+                data: formData,
                 type: 'post',
                 dataType: 'json',
-                beforeSend: function() {
+                cache: false,
+                contentType: false,
+                processData: false,
+
+                /** @inheritdoc */
+                beforeSend: function () {
                     if (self.isLoaderEnabled()) {
                         $('body').trigger(self.options.processStart);
                     }
                 },
-                success: function(res) {
+
+                /** @inheritdoc */
+                success: function (res) {
+                    var eventData, parameters;
+
+                    $(document).trigger('ajax:addToCart', {
+                        'sku': form.data().productSku,
+                        'productIds': productIds,
+                        'form': form,
+                        'response': res
+                    });
+
                     if (self.isLoaderEnabled()) {
                         $('body').trigger(self.options.processStop);
                     }
 
                     if (res.backUrl) {
-                        window.location = res.backUrl;
+                        eventData = {
+                            'form': form,
+                            'redirectParameters': []
+                        };
+                        // trigger global event, so other modules will be able add parameters to redirect url
+                        $('body').trigger('catalogCategoryAddToCartRedirect', eventData);
+
+                        if (eventData.redirectParameters.length > 0) {
+                            parameters = res.backUrl.split('#');
+                            parameters.push(eventData.redirectParameters.join('&'));
+                            res.backUrl = parameters.join('#');
+                        }
+
+                        self._redirect(res.backUrl);
+
                         return;
                     }
+
                     if (res.messages) {
                         $(self.options.messagesSelector).html(res.messages);
                     }
+
                     if (res.minicart) {
                         $(self.options.minicartSelector).replaceWith(res.minicart);
                         $(self.options.minicartSelector).trigger('contentUpdated');
                     }
+
                     if (res.product && res.product.statusText) {
                         $(self.options.productStatusSelector)
                             .removeClass('available')
@@ -53,10 +90,27 @@ define([
 
                     // HE Custom Ajax Content Updated Event
                     $(self.options.minicartSelector).trigger('ajaxContentUpdated');
+                },
+
+                /** @inheritdoc */
+                error: function (res) {
+                    $(document).trigger('ajax:addToCart:error', {
+                        'sku': form.data().productSku,
+                        'productIds': productIds,
+                        'form': form,
+                        'response': res
+                    });
+                },
+
+                /** @inheritdoc */
+                complete: function (res) {
+                    if (res.state() === 'rejected') {
+                        location.reload();
+                    }
                 }
             });
-        }
+        },
     });
 
-    return $.he.catalogAddToCart;
+    return $.mage.catalogAddToCart;
 });
